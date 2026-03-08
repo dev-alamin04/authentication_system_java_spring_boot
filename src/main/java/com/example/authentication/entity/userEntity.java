4a90951f3fd1411d66a751896a902018a1830bbd
Alamin648441 <aak75049@gmail.com>
2026-03-08 19:00:48 +0600

    inital commit

---
+++ src/main/java/com/example/authentication/entity/userEntity.java
@@ -0,0 +1,38 @@
+package com.example.authentication.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.management.relation.Role;
+
+@Entity
+@Data
+@Table(name = "users")
+public class userEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @Column(nullable = false)
+    private  String name ;
+    @Column(nullable = false,unique = true)
+    private  String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private boolean isVerified = false;
+    private boolean isActive = true;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    public  enum Role{
+        USER,ADMIN
+    }
+
+
+
+
+}
+
